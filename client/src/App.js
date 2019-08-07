d5cfcd08bdb8e63eb0431c792669855ad764ec4e
baestro <shwahoo@naver.com>
2019-08-07 16:33:15 +0900

    Add Get Customers REST API Modules

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,20 @@ const styles = theme =>({
   },
 })
 
-const customers = [
-  {
-    id:1,
-    image:'https://placeimg.com/64/64/1',
-    name:'bae',
-    birthday: '880214',
-    gender:'man',
-    job:'developer'
-  },
-  {
-    id:2,
-    image:'https://placeimg.com/64/64/2',
-    name:'so',
-    birthday: '910423',
-    gender:'official',
-    job:'Official'
-  },
-  {
-    id:3,
-    image:'https://placeimg.com/64/64/3',
-    name:'go',
-    birthday: '130330',
-    gender:'man',
-    job:'student'
-  }]
-
 class App extends Component{
+  state = {
+    customers:""
+  }
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render(){
     const {classes} = this.props;
     return(
@@ -63,7 +50,8 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c => {return (<Customer key={c.id} id={c.id} image ={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          {this.state.customers? this.state.customers.map(c => {return (<Customer key={c.id} id={c.id} image ={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+          }):""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message:"Hello Express!"});
+app.get('/api/customers',(req, res)=>{
+    res.send([
+        {
+          id:1,
+          image:'https://placeimg.com/64/64/1',
+          name:'bae',
+          birthday: '880214',
+          gender:'man',
+          job:'developer'
+        },
+        {
+          id:2,
+          image:'https://placeimg.com/64/64/2',
+          name:'so',
+          birthday: '910423',
+          gender:'official',
+          job:'Official'
+        },
+        {
+          id:3,
+          image:'https://placeimg.com/64/64/3',
+          name:'go',
+          birthday: '130330',
+          gender:'man',
+          job:'student'
+        }]);
 });
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
